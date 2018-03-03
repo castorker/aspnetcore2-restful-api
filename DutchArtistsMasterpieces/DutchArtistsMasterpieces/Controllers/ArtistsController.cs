26557d6e93a31883562463e38780f02638ff86f3
Eduardo <eduardo.cxcorreio@gmail.com>
2018-03-03 20:00:18 +0100

    Added Update Artist method - HTTP PUT request.

--- DutchArtistsMasterpieces/DutchArtistsMasterpieces/Controllers/ArtistsController.cs
+++ DutchArtistsMasterpieces/DutchArtistsMasterpieces/Controllers/ArtistsController.cs
@@ -32,6 +32,7 @@ namespace DutchArtistsMasterpieces.Controllers
             return Ok(artistToReturn);
         }
 
+        // http://localhost:50919/api/artists
         [HttpPost()]
         public IActionResult CreateArtist([FromBody] ArtistForCreationDto artist)
         {
@@ -83,6 +84,57 @@ namespace DutchArtistsMasterpieces.Controllers
             InMemoryDataStore.Current.Artists.Add(newArtist);
 
             return CreatedAtRoute("GetArtistById", new { artistId = newArtist.Id }, newArtist);
+        }
+
+        // http://localhost:50919/api/artists/6
+        [HttpPut("{artistId}")]
+        public IActionResult UpdateArtist(int artistId, [FromBody] ArtistForUpdateDto artist)
+        {
+            if (artist == null)
+            {
+                return BadRequest();
+            }
+
+            if (artist.ShortDescription == artist.Name)
+            {
+                ModelState.AddModelError("Short Description", "The provided short description should be different from the name.");
+            }
+
+            if (artist.LongDescription == artist.Name)
+            {
+                ModelState.AddModelError("Long Description", "The provided long description should be different from the name.");
+            }
+
+            if (artist.LongDescription == artist.ShortDescription)
+            {
+                ModelState.AddModelError("Long Description", "The provided long description should be different from the short description.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var artistToUpdate = InMemoryDataStore.Current.Artists.FirstOrDefault(a => a.Id == artistId);
+
+            if (artistToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            artistToUpdate.Name = artist.Name;
+            artistToUpdate.City = artist.City;
+            artistToUpdate.BirthYear = artist.BirthYear;
+            artistToUpdate.Birth = artist.Birth;
+            artistToUpdate.DeathYear = artist.DeathYear;
+            artistToUpdate.Death = artist.Death;
+            artistToUpdate.ShortDescription = artist.ShortDescription;
+            artistToUpdate.LongDescription = artist.LongDescription;
+            artistToUpdate.ImageUrl = artist.ImageUrl;
+            artistToUpdate.ImageThumbnailUrl = artist.ImageThumbnailUrl;
+            artistToUpdate.IsArtistOfTheMonth = (artist.Birth == null) ? false : (artist.Birth.Month == DateTime.Now.Month ? true : false);
+
+            return NoContent();
         }
     }
 }
---
+++ DutchArtistsMasterpieces/DutchArtistsMasterpieces/Models/ArtistForUpdateDto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchArtistsMasterpieces.Models
+{
+    public class ArtistForUpdateDto
+    {
+        [Required]
+        [StringLength(100, ErrorMessage = "You should provide a name value.")]
+        public string Name { get; set; }
+
+        [StringLength(50, ErrorMessage = "City can hold up to 100 chars.")]
+        public string City { get; set; }
+
+        [Range(0, 2018, ErrorMessage = "BirthYear must be a positive number with four digits length between 0 and 2018. e.g., 1645")]
+        public int BirthYear { get; set; }
+
+        [DataType(DataType.Date)]
+        [Range(typeof(DateTime), "", "1/1/2018")]
+        public DateTime Birth { get; set; }
+
+        [Range(0, 2018, ErrorMessage = "DeathYear must be a positive number with four digits length between 0 and 2018. e.g., 1645")]
+        public int DeathYear { get; set; }
+
+        [DataType(DataType.Date)]
+        [Range(typeof(DateTime), "", "1/1/2018")]
+        public DateTime Death { get; set; }
+
+        [StringLength(500, ErrorMessage = "Short description can hold up to 500 chars.")]
+        public string ShortDescription { get; set; }
+
+        [StringLength(5000, ErrorMessage = "Long description can hold up to 5000 chars.")]
+        public string LongDescription { get; set; }
+
+        public string ImageUrl { get; set; }
+        public string ImageThumbnailUrl { get; set; }
+
+        public bool IsArtistOfTheMonth { get; set; }
+    }
+}
