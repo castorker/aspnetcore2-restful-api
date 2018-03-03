52737b08e8621acf81321e38a410826c41f8ccd2
Eduardo <eduardo.cxcorreio@gmail.com>
2018-03-03 19:29:32 +0100

    Added Create Artist method - HTTP POST request.

--- DutchArtistsMasterpieces/DutchArtistsMasterpieces/Controllers/ArtistsController.cs
+++ DutchArtistsMasterpieces/DutchArtistsMasterpieces/Controllers/ArtistsController.cs
@@ -1,4 +1,5 @@
-﻿using DutchArtistsMasterpieces.Services;
+﻿using DutchArtistsMasterpieces.Models;
+using DutchArtistsMasterpieces.Services;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -29,6 +30,59 @@ namespace DutchArtistsMasterpieces.Controllers
             }
 
             return Ok(artistToReturn);
+        }
+
+        [HttpPost()]
+        public IActionResult CreateArtist([FromBody] ArtistForCreationDto artist)
+        {
+            if (artist == null)
+            {
+                return BadRequest();
+            }
+
+            if (artist.ShortDescription == artist.Name)
+            {
+                ModelState.AddModelError("Short Description", "The provided short description should be different from the name.");
+            }
+
+            if (artist.LongDescription == artist.Name)
+            {
+                ModelState.AddModelError("Long Description", "The provided long description should be different from the name.");
+            }
+
+            if (artist.LongDescription == artist.ShortDescription)
+            {
+                ModelState.AddModelError("Long Description", "The provided long description should be different from the short description.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // get the next artist Id - to be improved
+            var maxArtistId = InMemoryDataStore.Current.Artists.Max(i => i.Id);
+            var nextArtistId = ++maxArtistId;
+
+            var newArtist = new ArtistDto()
+            {
+                Id = nextArtistId,
+                Name = artist.Name,
+                City = artist.City,
+                BirthYear = artist.BirthYear,
+                Birth = artist.Birth,
+                DeathYear = artist.DeathYear,
+                Death = artist.Death,
+                ShortDescription = artist.ShortDescription,
+                LongDescription = artist.LongDescription,
+                ImageUrl = artist.ImageUrl,
+                ImageThumbnailUrl = artist.ImageThumbnailUrl,
+                IsArtistOfTheMonth = (artist.Birth == null) ? false : (artist.Birth.Month == DateTime.Now.Month ? true : false),
+            };
+
+            InMemoryDataStore.Current.Artists.Add(newArtist);
+
+            return CreatedAtRoute("GetArtistById", new { artistId = newArtist.Id }, newArtist);
         }
     }
 }
